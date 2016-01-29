a0f2678796d3a984796a0b95212032bae079793c
Jonathan Haber <jhaber@hubspot.com>
2016-01-29 17:18:11 -0500

    Bind as singleton, override shutdown

--- src/main/java/com/hubspot/dropwizard/guicier/JerseyServletModule.java
+++ src/main/java/com/hubspot/dropwizard/guicier/JerseyServletModule.java
@@ -7,22 +7,30 @@ import com.squarespace.jersey2.guice.BootstrapUtils;
 import org.glassfish.hk2.api.ServiceLocator;
 
 import javax.inject.Inject;
+import javax.inject.Singleton;
 
 public class JerseyServletModule extends ServletModule {
 
   @Override
   protected void configureServlets() {
-    ServiceLocator locator = new ServiceLocatorDecorator(BootstrapUtils.newServiceLocator());
+    ServiceLocator locator = new ServiceLocatorDecorator(BootstrapUtils.newServiceLocator()) {
+
+      @Override
+      public void shutdown() {
+        // don't shutdown, remove once jersey2-guice supports Jersey 2.21
+      }
+    };
 
     install(new BootstrapModule(locator));
 
     bind(HK2Linker.class);
   }
 
+  @Singleton
   public static class HK2Linker {
 
     @Inject
-    public HK2Linker(Injector injector,ServiceLocator locator) {
+    public HK2Linker(Injector injector, ServiceLocator locator) {
       BootstrapUtils.link(locator, injector);
       BootstrapUtils.install(locator);
     }
