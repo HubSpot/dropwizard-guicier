7b58da5ceff77b49aa6a4423ceb45a3094378dc8
Jonathan Haber <jhaber@hubspot.com>
2016-06-02 14:52:50 -0400

    Make it actually work, remove a bunch of dead code

--- src/main/java/com/hubspot/dropwizard/guicier/GuiceBundle.java
+++ src/main/java/com/hubspot/dropwizard/guicier/GuiceBundle.java
@@ -1,30 +1,22 @@
 package com.hubspot.dropwizard.guicier;
 
 import static com.google.common.base.Preconditions.checkNotNull;
-import static com.google.common.base.Preconditions.checkState;
 
 import java.util.Arrays;
 
-import javax.inject.Inject;
-import javax.servlet.Servlet;
-
-import org.glassfish.jersey.server.ResourceConfig;
-import org.glassfish.jersey.servlet.ServletContainer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.common.base.Function;
 import com.google.common.collect.ImmutableSet;
 import com.google.inject.Binder;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Module;
-import com.google.inject.Scopes;
 import com.google.inject.Stage;
-import com.google.inject.TypeLiteral;
-import com.google.inject.name.Named;
-import com.google.inject.name.Names;
 import com.google.inject.servlet.GuiceFilter;
+import com.google.inject.servlet.ServletModule;
+import com.squarespace.jersey2.guice.JerseyGuiceModule;
+import com.squarespace.jersey2.guice.JerseyGuiceUtils;
 
 import io.dropwizard.Configuration;
 import io.dropwizard.ConfiguredBundle;
@@ -35,9 +27,6 @@ import io.dropwizard.setup.Environment;
  * @author <a href="mailto:henning@schmiedehausen.org">Henning P. Schmiedehausen</a>
  */
 public class GuiceBundle<T extends Configuration> implements ConfiguredBundle<T> {
-  private static final String GUICE_BUNDLE_NAME = "_guice_bundle";
-  private static final Named GUICE_BUNDLE_NAMED = Names.named(GUICE_BUNDLE_NAME);
-
   private static final Logger LOG = LoggerFactory.getLogger(GuiceBundle.class);
 
   public static <U extends Configuration> Builder<U> defaultBuilder(final Class<U> configClass) {
@@ -50,10 +39,6 @@ public class GuiceBundle<T extends Configuration> implements ConfiguredBundle<T>
   private final Stage guiceStage;
 
   private Bootstrap<?> bootstrap = null;
-
-  @Inject
-  @Named(GUICE_BUNDLE_NAME)
-  private volatile Function<ResourceConfig, Servlet> replacer = null;
 
   private GuiceBundle(final Class<T> configClass,
                       final ImmutableSet<Module> guiceModules,
@@ -87,45 +72,21 @@ public class GuiceBundle<T extends Configuration> implements ConfiguredBundle<T>
                         .addAll(guiceModules)
                         .addAll(dropwizardAwareModules)
                         .add(new GuiceEnforcerModule())
-                        .add(new JerseyServletModule())
+                        .add(new ServletModule())
+                        .add(new JerseyGuiceModule("__HK2_Generated_0"))
                         .add(dropwizardModule)
                         .add(new Module() {
                           @Override
                           public void configure(final Binder binder) {
                             binder.bind(Environment.class).toInstance(environment);
                             binder.bind(configClass).toInstance(configuration);
-
-                            binder.bind(ServletContainer.class).toInstance(new DropwizardGuiceContainer(environment));
-
-                            binder.bind(new TypeLiteral<Function<ResourceConfig, Servlet>>() {
-                            })
-                                    .annotatedWith(GUICE_BUNDLE_NAMED)
-                                    .to(GuiceContainerReplacer.class)
-                                    .in(Scopes.SINGLETON);
                           }
                         }).build());
-
-    injector.injectMembers(this);
-    checkState(replacer != null, "No guice container replacer was injected!");
+    JerseyGuiceUtils.install(injector);
 
     dropwizardModule.register(environment, injector);
 
-    environment.jersey().replace(replacer);
     environment.servlets().addFilter("Guice Filter", GuiceFilter.class).addMappingForUrlPatterns(null, false, environment.getApplicationContext().getContextPath() + "*");
-  }
-
-  private static class GuiceContainerReplacer implements Function<ResourceConfig, Servlet> {
-    private final ServletContainer container;
-
-    @Inject
-    GuiceContainerReplacer(final ServletContainer container) {
-      this.container = checkNotNull(container, "container is null");
-    }
-
-    @Override
-    public Servlet apply(final ResourceConfig resourceConfig) {
-      return container;
-    }
   }
 
   public static class GuiceEnforcerModule implements Module {
